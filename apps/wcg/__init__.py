15cd8b274b22713b97ce2cdfabe7e7e8769b4247
waynegregory <waynecgregory@gmail.com>
2024-08-06 21:34:07 +0000

    creating an app tutorial

---
+++ apps/wcg/__init__.py
@@ -0,0 +1,17 @@
+import datetime
+from py4web import action
+
+
+@action("index")
+def index():
+    return f"Hello World @ {datetime.datetime.now()}"
+
+@action('colors')
+def colors():
+    return {'colors': ['red', 'blue', 'green']}
+
+@action('color/<name>')
+def color(name):
+    if name in ['red', 'blue', 'green']:
+        return 'You picked color %s' % name
+    return 'Unknown color %s' % name
