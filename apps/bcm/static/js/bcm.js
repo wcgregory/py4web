d2f4ae5d1b37201ca0f000f01afdb72723cd391e
waynegregory <129503132+wcgregory@users.noreply.github.com>
2024-09-13 21:16:33 +0000

    adjusted button name

--- apps/bcm/static/js/bcm.js
+++ apps/bcm/static/js/bcm.js
@@ -2,14 +2,14 @@ let app = {}
 
 let init = (app) => {
   app.data = {}
-  app.bcm_run_commands_button = function(device_id) {
+  app.bcm_button_run_cmds = function(device_id) {
     console.log("Test - Button Pressed")
     let url="/bcm/run_commands/" + device_id
     axios.get(url).then(function(response) {
       console.log(response.data)
     })
   }
-  app.methods = {bcm_run_commands_button: app.bcm_run_commands_button}
+  app.methods = {bcm_button_run_cmds: app.bcm_button_run_cmds}
   app.vue = new Vue({
     el: "#vue-target",
     data: app.data,
